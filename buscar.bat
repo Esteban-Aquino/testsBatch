50fa411f36bc2718cab5a248363ec0a58d4c00b4
Mefistoel <eaquinof@gmail.com>
2020-10-01 11:37:36 -0400

    Abrir calculadora completo

--- buscar.bat
+++ buscar.bat
@@ -5,10 +5,11 @@ tasklist /FI "IMAGENAME eq calc.exe" 2>NUL | find /I /N "calc.exe">NUL
 if "%ERRORLEVEL%"=="0" (goto corridendo) else (goto nocorriendo)
 
 :corridendo
-echo Program is running
-timeout /t 5 >nul
+echo El programa se esta ejecutando
+timeout /t 3 >nul
 exit
 
 :nocorriendo
+echo Iniciando el programa
 start calc.exe&exit
 exit
